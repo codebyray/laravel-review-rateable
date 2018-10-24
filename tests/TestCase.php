43ef8a95762f83253182f57be4de45fa385a8a8b
CodebyRay <dev@codebyray.com>
2018-10-24 02:32:51 -0700

    Modified composer.json file for auto loading

---
+++ tests/TestCase.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace CodebyRay\ReviewRateable\Test;
+
+use CodebyRay\ReviewRateable\ReviewRateableServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
+
+abstract class TestCase extends Orchestra
+{
+
+    /**
+     * Setup the test environment.
+     */
+    public function setUp()
+    {
+        parent::setUp();
+    }
+
+    /**
+     * add the package provider
+     *
+     * @param $app
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [ReviewRateableServiceProvider::class];
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        // Setup default database to use sqlite :memory:
+        $app['config']->set('database.default', 'testing');
+        $app['config']->set('database.connections.testing', [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+            'prefix'   => '',
+        ]);
+    }
+}
